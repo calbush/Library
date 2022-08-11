262990196900861ee74db2cf309df066a624f707
Caleb Bush <calebmatthewbush@gmail.com>
2022-08-11 17:45:54 -0500

    Fix issue causing books with identical page counts to be removed

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ libraryTable.style.display = 'none';
 let submitBtn = document.getElementById('add');
 const bookForm = document.getElementById('addBookForm');
 
-//bookForm.style.display = 'none';
+bookForm.style.display = 'none';
 
 const bdy = document.querySelector('body')
 
 const bookBtn = document.querySelector('#addBookBtn');
+
+let submitInitiator = true;
 
 //Book Constructor
 function Book(title, author, pages, read) {
@@ -21,15 +23,18 @@ function Book(title, author, pages, read) {
 }
 //User hits submit button, create book object and add to library
 function createLibraryEntry(){
+    if (submitInitiator){
     submitBtn.addEventListener('click', () => {
+        submitInitiator = false;
         let title = document.getElementById('title').value;
         let author = document.getElementById('author').value;
         let pages = document.getElementById('pages').value;
         let readStatus = document.querySelector('input[name="readStatus"]:checked').value;
         myLibrary.push(new Book(title, author, pages, readStatus));
-        //bookForm.style.display = 'none';
+        bookForm.style.display = 'none';
         addBooktoTable();
     })
+    }
 }
 
 //Function to add Book objects as DOM elements; Uses last element in myLibrary array, called upon new object being added to myLibrary.
@@ -57,10 +62,11 @@ function addBooktoTable(){
 
         let tblRemove = document.createElement('button');
         libraryTable.appendChild(tblRemove);
-        tblRemove.classList.add('remove', myLibrary[book].pages, 'delete');
+        tblRemove.classList.add('delete');
+        tblRemove.setAttribute('data-iteration', `${book}`)
         tblRemove.textContent = 'Remove';
         tblRemove.addEventListener('click', (e) => {
-            removeBook(e.target.classList[1]); 
+            removeBook(e.target.dataset.iteration); 
         })
     }
 }
@@ -73,37 +79,38 @@ function removeBook(removeBtnClass) {
     }
 
     for (let i = 0; i < myLibrary.length; i++){
-        if (removeBtnClass == myLibrary[i].pages){
+        if (removeBtnClass == i){
             myLibrary.splice(i, 1);
         }
     }
-    for (book of myLibrary){
+    for (let iter = 0; iter < myLibrary.length; iter++){
         let tblTitle = document.createElement('div');
         tblTitle.classList.add('delete');
         libraryTable.appendChild(tblTitle);
-        tblTitle.textContent = book.title;
+        tblTitle.textContent = myLibrary[iter].title;
 
         let tblAuthor = document.createElement('div');
         tblAuthor.classList.add('delete');
         libraryTable.appendChild(tblAuthor);
-        tblAuthor.textContent = book.author;
+        tblAuthor.textContent = myLibrary[iter].author;
 
         let tblPages = document.createElement('div');
         tblPages.classList.add('delete');
         libraryTable.appendChild(tblPages);
-        tblPages.textContent = book.pages;
+        tblPages.textContent = myLibrary[iter].pages;
 
         let tblRead = document.createElement('div');
         tblRead.classList.add('delete');
         libraryTable.appendChild(tblRead);
-        tblRead.textContent = book.read;
+        tblRead.textContent = myLibrary[iter].read;
 
         let tblRemove = document.createElement('button');
         libraryTable.appendChild(tblRemove);
-        tblRemove.classList.add('remove', book.pages, 'delete');
+        tblRemove.classList.add('delete');
+        tblRemove.setAttribute('data-iteration', `${iter}`);
         tblRemove.textContent = 'Remove';
         tblRemove.addEventListener('click', (e) => {
-            removeBook(e.target.classList[1]); 
+            removeBook(e.target.dataset.iteration); 
         })
     }
 }
@@ -111,7 +118,7 @@ function removeBook(removeBtnClass) {
 //User hits new book button, form is displayed
 bookBtn.addEventListener('click', (e) => {
     if (e.target.innerHTML === '+ New book') {
-        //bookForm.style.display = 'block';
+        bookForm.style.display = 'block';
         createLibraryEntry();
     }
 })
